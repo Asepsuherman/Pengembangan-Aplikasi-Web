36f9cccaaec45bf2e7d7f367e16ce2c2bbe595eb
Asep <asepsuherman389@gmail.com>
2023-10-12 03:35:11 +0700

    Update MstPangkat (Controller & Models)

--- app/Http/Controllers/MstPangkatController.php
+++ app/Http/Controllers/MstPangkatController.php
@@ -3,15 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\MstPangkat;
+
+/**
+* Class MstPangkatController
+* @package App\Http\Controllers
+*/
 
 class MstPangkatController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    * Display a listing of the resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
+    public function index(Request $request)
     {
-        //
+        $search = \Request::get('search');
+        $p = MstPangkat::paginate();
+        $mstPangkat = \DB::table('mst_pangkat')
+        ->where('nama_pangkat','LIKE','%'.$search.'%')
+        ->orWhere('pangkat_gol','LIKE', '%'.$search.'%')
+        ->paginate($p->perPage());
+
+        return view('mst-pangkat.index', compact('mstPangkat'))
+        ->with('i', (request()->input('page', 1) - 1) * $p->perPage());
     }
 
     /**
--- app/Models/MstPangkat.php
+++ app/Models/MstPangkat.php
@@ -1,11 +1,28 @@
 <?php
-
 namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class MstPangkat extends Model
 {
-    use HasFactory;
+ protected $table = 'mst_pangkat';
+ protected $primaryKey = 'id';
+
+ static $rules = ['nama_pangkat' => 'required',
+ 'pangkat_gol' => 'required'];
+ protected $perPage = 10;
+ /**
+ * Attributes that should be mass-assignable.
+ *
+ * @var array
+ */
+ protected $fillable = ['nama_pangkat','pangkat_gol'];
+ /**
+ * @return \Illuminate\Database\Eloquent\Relations\HasMany
+ */
+ /*public function riwayatPangkats()
+ {
+ return $this->hasMany('App\Models\RiwayatPangkat', 'mst_pangkat_id', 'id');
+ }*/
+
 }

cc4473f78a13abb0b74fc96a7c96e2bd18b193d9
Asep <asepsuherman389@gmail.com>
2023-10-12 03:17:08 +0700

    Create MstPangkat models

---
+++ app/Models/MstPangkat.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MstPangkat extends Model
+{
+    use HasFactory;
+}
